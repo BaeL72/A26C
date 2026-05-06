171d53185a6925c363aa79327b690585edcef925
BaeL72 <barbiefromtheslums@gmail.com>
2026-05-06 22:26:20 +0200

    NeoConfig 2.3.1: GitSigns another design

--- nvim/lua/plugins/gitsigns.lua
+++ nvim/lua/plugins/gitsigns.lua
@@ -3,20 +3,26 @@ return {
 	config = function()
 		require("gitsigns").setup({
 			signs = {
-				add = { text = " ┃" },
-				change = { text = " ┃" },
-				delete = { text = " _" },
-				topdelete = { text = " ‾" },
+				-- add = { text = " ┃" },
+				-- change = { text = " ┃" },
+				-- delete = { text = " _" },
+				-- topdelete = { text = " ‾" },
+				-- changedelete = { text = " ~" },
+				-- untracked = { text = " ┆" },
+				add = { text = " +" },
+				change = { text = " ~" },
+				delete = { text = " -" },
+				topdelete = { text = " -" },
 				changedelete = { text = " ~" },
-				untracked = { text = " ┆" },
+				untracked = { text = " ?" },
 			},
 			signs_staged = {
-				add = { text = " ┃" },
-				change = { text = " ┃" },
-				delete = { text = " _" },
-				topdelete = { text = " ‾" },
+				add = { text = " +" },
+				change = { text = " ~" },
+				delete = { text = " -" },
+				topdelete = { text = " -" },
 				changedelete = { text = " ~" },
-				untracked = { text = " ┆" },
+				untracked = { text = " ?" },
 			},
 			signs_staged_enable = true,
 			signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
@@ -51,5 +57,17 @@ return {
 				col = 1,
 			},
 		})
+		vim.api.nvim_set_hl(0, "GitsignsAdd", { fg = "#00ff00" })
+		vim.api.nvim_set_hl(0, "GitsignsChange", { fg = "#ffff00" })
+		vim.api.nvim_set_hl(0, "GitsignsDelete", { fg = "#ff0000" })
+		vim.api.nvim_set_hl(0, "GitsignsChangedelete", { fg = "#ffff00" })
+		vim.api.nvim_set_hl(0, "GitSignsTopdelete", { fg = "#ff0000" })
+		vim.api.nvim_set_hl(0, "GitSignsUntracked", { fg = "#00ffff" })
+		vim.api.nvim_set_hl(0, "GitsignsStagedAdd", { fg = "#00ff00" })
+		vim.api.nvim_set_hl(0, "GitsignsStagedChange", { fg = "#ffff00" })
+		vim.api.nvim_set_hl(0, "GitsignsStagedDelete", { fg = "#ff0000" })
+		vim.api.nvim_set_hl(0, "GitsignsStagedChangedelete", { fg = "#ffff00" })
+		vim.api.nvim_set_hl(0, "GitSignsStagedTopdelete", { fg = "#ff0000" })
+		vim.api.nvim_set_hl(0, "GitSignsStagedUntracked", { fg = "#00ffff" })
 	end,
 }
